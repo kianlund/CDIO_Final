32182a70115655ee5172ab1cbef47ae7341b1e46
Thor <tgregersen998@gmail.com>
2020-11-24 13:50:49 +0100

    Added dicecup test

---
+++ src/test/java/game/DiceCupTest.java
@@ -0,0 +1,36 @@
+package game;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DiceCupTest {
+    private int testNumberOfDice;
+    private int testSum;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void rollDice() {
+    }
+
+    @Test
+    void getNumberOfDice() {
+    }
+
+    @Test
+    void getDiceinCup() {
+    }
+
+    @Test
+    void getSum() {
+    }
+}
