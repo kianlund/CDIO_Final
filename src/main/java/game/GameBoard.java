21dd64591e02cf8c4c5a83010fafc592dae50ed9
Kian Lund <lundk90@gmail.com>
2021-01-12 11:50:09 +0100

    Rent implementation

--- src/main/java/game/GameBoard.java
+++ src/main/java/game/GameBoard.java
@@ -24,27 +24,41 @@ public class GameBoard {
             0,7000,0,8000};
 
     private final int[][] houseRent ={
-            {50, 250, 750, 2250, 4000, 6000},
-            {100,600,1800,5400,8000,11000,150,600,2000,6000,9000,12000},
-            {200,1000,3000,9000,12500,15000,250,1250,3750,10000,14000,18000},
-            {300,1400,4000,11000,15000,19000,350,1600,4400,12000,16000,20000},
-            {350,1800,5000,14000,17500,21000,400,2000,6000,15000,18500,22000},
-            {450,2200,6600,16000,19500,23000,500,2400,7200,17000,20500,24000},
-            {550,2600,7800,18000,220000,25000,600,3000,9000,20000,24000,28000},
-            {700,3500,10000,22000,26000,30000,1000,4000,12000,28000,34000,40000},
-            {500,1000,2000,4000},
+            {50,250,750,2250,4000,6000},
+            {50,250,750,2250,4000,6000},
+            {100,600,1800,5400,8000,11000},
+            {100,600,1800,5400,8000,11000},
+            {150,600,2000,6000,9000,12000},
+            {200,1000,3000,9000,12500,15000},
+            {200,1000,3000,9000,12500,15000},
+            {250,1250,3750,10000,14000,18000},
+            {300,1400,4000,11000,15000,19000},
+            {300,1400,4000,11000,15000,19000},
+            {350,1600,4400,12000,16000,20000},
+            {350,1800,5000,14000,17500,21000},
+            {350,1800,5000,14000,17500,21000},
+            {400,2000,6000,15000,18500,22000},
+            {450,2200,6600,16000,19500,23000},
+            {450,2200,6600,16000,19500,23000},
+            {500,2400,7200,17000,20500,24000},
+            {550,2600,7800,18000,22000,25000},
+            {550,2600,7800,18000,22000,25000},
+            {600,3000,9000,20000,24000,28000},
+            {700,3500,10000,22000,26000,30000},
+            {1000,4000,12000,28000,34000,40000}
     };
     private final Color[] colorArray = {Color.BLUE, Color.ORANGE, Color.YELLOW, Color.GRAY, Color.RED, Color.WHITE, Color.PINK, Color.CYAN};
     private int[][] colorArr;
     private int colorCounter;
     private int tileCounter;
-    private int arraySize;
+    private int rentCounter;
 
     private Language tileStrings = new Language("dkTileStrings.txt");
 
     public GameBoard (int numOfTiles, GUI_Field[] gui_fields) {
         tiles = new Tile[numOfTiles];
         colorCounter = 0;
+        rentCounter = 0;
         tileCounter = 1;
         //tiles[0] = new Tile();
         for (int i = 0; i < numOfTiles; i++) {
@@ -56,6 +70,8 @@ public class GameBoard {
                 if (gui_fields[i] instanceof GUI_Street) {
                     tiles[i].setTileColor(colorArray[colorCounter]);
                     tiles[i].getGui_field().setBackGroundColor(colorArray[colorCounter]);
+                    tiles[i].setRent(houseRent[rentCounter]);
+                    rentCounter++;
                     tileCounter++;
                     if (tileCounter == 3) {tileCounter = 0; colorCounter++;}
                 }
