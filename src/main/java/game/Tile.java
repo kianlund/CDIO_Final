5992bffacfe4ff9c1680437c36b5ee48718a8347
Kian Lund <lundk90@gmail.com>
2021-01-12 11:49:51 +0100

    Implemented get-/setRent

--- src/main/java/game/Tile.java
+++ src/main/java/game/Tile.java
@@ -1,6 +1,5 @@
 package game;
 
-import com.company.Main;
 import gui_fields.*;
 
 import java.awt.*;
@@ -10,31 +9,40 @@ public class Tile {
     private Player owner;
     private final int number;
     private String description;
-    private int rent;
+    private int price;
+    private int[] rent;
     private GUI_Field gui_field;
     private Color tileColor;
 
     public Tile (int number1, String description1, int theRent, GUI_Field gui_field1){
         this.number=number1;
         description=description1;
-        rent =theRent;
+        price =theRent;
         gui_field=gui_field1;
         gui_field.setTitle(description);
         gui_field.setSubText(Integer.toString(number));
         owner = null;
     }
 
-    public Tile (int theRent, GUI_Field gui_field1, int num){
-        rent =theRent;
-        gui_field=gui_field1;
-        tileColor=null;
-        this.number=num;
+    public Tile (int priceInput, GUI_Field gui_field1, int num){
+        price = priceInput;
+        rent = null;
+        gui_field = gui_field1;
+        tileColor = null;
+        this.number = num;
         owner = null;
     }
 
     public int getPrice() {
+        return price;
+    }
 
+    public int[] getRent() {
         return rent;
+    }
+
+    public void setRent(int[] input) {
+        rent = input;
     }
 
     public Player getOwner() {
@@ -45,7 +53,7 @@ public class Tile {
         this.owner = owner;
         if (gui_field instanceof GUI_Ownable) {
             ((GUI_Ownable) gui_field).setOwnerName(owner.getName());
-            ((GUI_Ownable) gui_field).setRent(Integer.toString(rent));
+            ((GUI_Ownable) gui_field).setRent(Integer.toString(price));
         }
 
     }
